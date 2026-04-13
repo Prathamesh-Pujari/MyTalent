85f307d1f341d5e889ccc901d9031ef14a80fcb8
prathamesh <prathamesh_pujari@outlook.com>
2026-04-13 13:07:51 +0530

    implementing server side auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,12 @@ import { fileURLToPath } from "url";
 import { ENV } from "./lib/env.js";
 import helmet from "helmet";
 import { connectDB } from "./lib/db.js";
-import cors from "cors"
+import cors from "cors";
 import { serve } from "inngest/express";
 import { inngest } from "./lib/inngest.js";
 import { functions } from "./lib/inngest.js";
-import { clerkMiddleware } from '@clerk/express'
-
-
-
+import { clerkMiddleware } from "@clerk/express";
+import chatRoutes from "./routes/chatRoutes.js"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -19,18 +17,18 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 //middleware
-app.use(express.json())
+app.use(express.json());
 // credentials true = server allow browser to include cookies on request
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }))
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth fields to req obj
 
-app.use("/api/inngest", serve({ client: inngest, functions }))
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.use("/api/chat", chatRoutes);
 
 app.get("/health", (req, res) => {
+  req.auth;
   res.status(200).json({ msg: "success from health endpoint" });
-});
-
-app.get("/books", (req, res) => {
-  res.status(200).json({ msg: "success from books endpoint" });
 });
 
 if (ENV.NODE_ENV === "production") {
@@ -41,16 +39,15 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-
-
 const startServer = async () => {
   try {
-    await connectDB()
-    app.listen(ENV.PORT, () => console.log("Server is running on port : ", ENV.PORT),
+    await connectDB();
+    app.listen(ENV.PORT, () =>
+      console.log("Server is running on port : ", ENV.PORT),
     );
   } catch (error) {
-    console.log("Error Starting Server", error)
+    console.log("Error Starting Server", error);
   }
-}
+};
 
-startServer(); 
+startServer();
